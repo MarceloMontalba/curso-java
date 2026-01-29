52bb4441cd2232c0fcef333bae03a7df3cd06892
Marcelo Montalba <mmontalbapavez@gmail.com>
2026-01-29 00:11:57 -0300

    Conversion de tipos de datos
    Utilización de metodos de transformación de datos de las clases wrapper de los tipos de datos primitivos que permiten transformar el tipo de valor que se está manejando.

--- HolaMundo/src/ConversionDeTipos.java
+++ HolaMundo/src/ConversionDeTipos.java
@@ -5,8 +5,41 @@ public class ConversionDeTipos {
         int numeroInt = Integer.parseInt(numeroStr);
         System.out.println("numeroInt: "+numeroInt);
 
-        String realStr = "98231.23e-2"; // Notación de punto flotante
+        String realStr = "98231.23e-2"; // Notación de cientifica/punto flotante
         double realDouble = Double.parseDouble(realStr);
         System.out.println("realDouble: "+realDouble);
+
+        String logicoStr = "true";
+        // String logicoStr = "1";   // Java no lo transforma a boolean. Así que se setea en falso
+        // String logicoStr = "TruE";   // Java lo procesará como boolean verdadera, ya que el pase no hara caso al CASE.
+        boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
+        System.out.println("logicoBoolean: "+logicoBoolean);
+
+        var numeroStrVar = "31";
+
+        var numeroIntVar = Integer.parseInt(numeroStrVar);
+        System.out.println("numeroIntVar: "+numeroIntVar);
+
+        int otroNumeroInt = 100;
+
+        System.out.println("otroNumeroInt: "+ otroNumeroInt);
+
+        String otroNumeroStr = Integer.toString(otroNumeroInt);
+        System.out.println("otroNumeroStr: "+ otroNumeroStr);
+
+        // Otra forma de convertir a string
+        int otroNumeroInt2 = 140;
+
+        System.out.println("otroNumeroInt2: "+ otroNumeroInt2);
+
+        String otroNumeroStr2 = String.valueOf(otroNumeroInt2);
+        System.out.println("otroNumeroStr2: "+ otroNumeroStr2);
+        
+        double otroRealDouble = 123123.23e-4;
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        otroRealStr = String.valueOf(3213123.34F);
+        System.out.println("otroRealStr = " + otroRealStr);
     }
 }
