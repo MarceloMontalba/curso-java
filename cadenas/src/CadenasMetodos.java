a5cf2b90c0e4a1da2807e8d3e4927fe465e5c840
Marcelo Montalba <mmontalbapavez@gmail.com>
2026-01-31 19:04:11 -0300

    Metodos utiles de la clase String

---
+++ cadenas/src/CadenasMetodos.java
@@ -0,0 +1,18 @@
+public class CadenasMetodos {
+    public static void main(String[] args) {
+        String nombre = "Marcelo";
+
+        System.out.println("nombre.length() = " +nombre.length());
+        System.out.println("nombre.toUpperCase() = "+nombre.toUpperCase());
+        System.out.println("nombre.toLowerCase() = " + nombre.toLowerCase());
+        System.out.println("nombre.equals(\"Marcelo\") = "+nombre.equals("Marcelo"));
+        System.out.println("nombre.equals(\"marcelo\") = "+nombre.equals("marcelo"));
+        System.out.println("nombre.equalsIgnoreCase(\"Marcelo\") = "+nombre.equalsIgnoreCase("Marcelo"));
+        System.out.println("nombre.equalsIgnoreCase(\"marcelo\") = "+nombre.equalsIgnoreCase("marcelo"));
+        System.out.println("nombre.compareTo(\"Juan\") = "+nombre.compareTo("Juan")); // 0 si son iguales; negativo si nombre tiene viene antes alfabeticamente o lexicograficamente; positivo si nombre viene despues
+        System.out.println("nombre.compareTo(\"Marcelo\") = "+nombre.compareTo("Marcelo"));
+        System.out.println("nombre.charAt(0) = "+ nombre.charAt(0)); // Consige el caracter de la posicion dada
+        System.out.println("nombre.substring(2) = "+nombre.substring(2));
+        System.out.println("nombre.substring(1,7) = "+nombre.substring(1,7)); // El limite no es inclusive (ese caracter no está considerado)
+    }
+}
